fbfb26de50e2581af71d0ef671b062d5bf1b4811
aaron <aaronjondorney@gmail.com>
2020-08-12 17:12:52 +0100

    create GUI using tkinter

--- amazon_wishlist/testing.py
+++ amazon_wishlist/testing.py
@@ -1,57 +1,59 @@
 import shelve
 import os
 import requests
-
-def output():
-    x=os.system("scrapy crawl book_depository")
-    # process=CrawlerProcess()
-    # process.crawl(book_depository_spider)
-    # process.start()
-    db=shelve.open("list")
-    for item in db:
-        var=db[item]
-        print(var.name+"\t| current:€"+str(var.price)+"\t| avg:€"+str(var.avg)+"\t| min:€"+str(var.lowest))
-    db.close()
-def add():
-    url=input("url? ")
-    check=requests.get(url)
+from tkinter import *
+wishlist=Tk()
+def add(url):
+    try:
+        check=requests.get(url)
+    except:
+        print("not a valid url")
+        return
     if check.ok and "bookdepository" in url:
         with open("urls.txt","a") as urls:
-            urls.write(url)
-            print("item added to list")
+            urls.write(url+"\n")
+        update()
 
     else:
         print("url not valid")
-def remove():
-    numbers={}
-    pointer=0
+def remove(row,item):
     db=shelve.open("list")
-    for item in db:
-        pointer+=1
-        profile=db[item]
-        print(str(pointer)+": "+profile.name + "\t| current:€" + str(profile.price) + "\t| avg:€" + str(profile.avg) + "\t| min:€" + str(profile.lowest))
-        numbers[pointer]=profile
-    resonse=int(input("select the number you wish to remove: "))
-    if numbers.get(resonse):
-        temp=numbers[resonse]
-        print(temp.name+" removed")
-        with open("urls.txt", "r") as url:
-            urls=url.readlines()
-        with open("urls.txt","w") as new:
-            for url in urls:
-                if url.strip("\n")!=temp.link:
-                    new.write(url)
-        del db[temp.name]
-        db.close()
-    else:
-        print("invalid input")
+    with open("urls.txt", "r") as url:
+        urls=url.readlines()
+    with open("urls.txt","w") as new:
+        for url in urls:
+            if url.strip("\n")!=item.link:
+                new.write(url)
+    del db[item.name]
+    db.close()
+    for label in wishlist.grid_slaves():
+        if int(label.grid_info()["row"]==row):
+            label.grid_forget()
 
-responses={"output":"output()","add":"add()","remove":"remove()"}
-while True:
-    user_in=input("input?: ")
-    if user_in=="exit":
-        break
-    elif responses.get(user_in):
-        exec(responses[user_in])
-    else:
-        print("unrecognised command")
+class geo():
+    def __init__(self,profile,row):
+        self.profile=profile
+        self.row=row
+def update():
+    os.system("scrapy crawl book_depository")
+    db = shelve.open("list")
+    row = 0
+    widgets=wishlist.grid_slaves()
+    buttons=[]
+    for item in widgets:
+        item.destroy()
+    for item in db:
+        var=db[item]
+        Label(master=wishlist,text=var.name+"\t").grid(row=row,column=0)
+        Label(master=wishlist,text= "|current:€"+str(var.price)+"\t|").grid(row=row,column=1)
+        Label(master=wishlist,text= "avg:€"+str(var.avg)+"\t|").grid(row=row,column=2)
+        Label(master=wishlist,text= "min:€"+str(var.lowest)).grid(row=row,column=3)
+        buttons.append(Button(master=wishlist,text="remove",command=lambda i=row ,item=var: remove(i,item)))
+        buttons[row].grid(row=row,column=4)
+        row+=1
+    entry1=Entry(wishlist)
+    entry1.grid(row=row,column=1)
+    Button(wishlist,text="add",command=lambda: add(entry1.get())).grid(row=row+1,column=1)
+    db.close()
+update()
+wishlist.mainloop()
